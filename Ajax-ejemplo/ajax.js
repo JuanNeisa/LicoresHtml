2323d1af68d0ad6c89f7d3eab4ca5d11a1a7cc1b
Juan David Neisa Paramo <jdneisap@correo.udistrital.edu.co>
2021-01-09 23:06:17 -0500

    Metodo post y get

--- Ajax-ejemplo/ajax.js
+++ Ajax-ejemplo/ajax.js
@@ -1,5 +1,8 @@
 //Obtener un elemento del DOM mediante el id
 var lista = document.getElementById("listado");
+var boton = document.getElementById("boton");
+
+
 
 function reqListener () {
     //La peticion llega como texto en el momento que se usa 'responseText'
@@ -8,6 +11,7 @@ function reqListener () {
     //Se puede castear los objetos a formato JSON
     //Ademas se puede adignar el objeto a una variable
     var usuarios =JSON.parse(this.responseText);
+    console.log(usuarios)
 
     //Mapeadno el objeto JSON para listarlo
     var listaUsuarios = usuarios.map(x => `<li>${x.nombre}</li>`).join('');
@@ -19,8 +23,29 @@ function reqListener () {
 
   }
   
-  var oReq = new XMLHttpRequest();
-  oReq.addEventListener("load", reqListener);
-  //oReq.open("[Tipo_Peticion]", "Direccion_Servidor");
-  oReq.open("GET", "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
-  oReq.send();
+  //Crear la variable tipo 'XMLHttpRequest'
+  var peticion = new XMLHttpRequest();
+  //Listener para que cada que cargue el documento realice el callback 'reqListener'
+  peticion.addEventListener("load", reqListener);
+  this.refrescar();
+  
+function enviarDatos() {
+  peticion.open(
+    "POST",
+    "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios",
+    true
+  );
+  peticion.setRequestHeader(
+    "Content-type",
+    "application/x-www-form-urlencoded"
+  );
+  peticion.send("nombre=LUNES24");
+  setTimeout(refrescar, 1000);
+}
+
+function refrescar() {
+  peticion.open("GET", "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
+  peticion.send();
+}
+
+boton.onclick = enviarDatos;
