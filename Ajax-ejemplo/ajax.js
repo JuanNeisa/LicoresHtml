270b95afdd40a448dda426aef4d57ea081639258
Juan David Neisa Paramo <jdneisap@correo.udistrital.edu.co>
2021-01-09 22:18:49 -0500

    Casteo a objeto tipo JSON y obtener elementos del SOM mediante id

--- Ajax-ejemplo/ajax.js
+++ Ajax-ejemplo/ajax.js
@@ -1,6 +1,12 @@
+//Obtener un elemento del DOM mediante el id
+var lista = document.getElementById("listado");
+
 function reqListener () {
     //La peticion llega como texto en el momento que se usa 'responseText'
-    console.log(this.responseText);
+    //console.log(this.responseText);
+
+    //Se puede castear los objetos a formato JSON
+    console.log(JSON.parse(this.responseText));
   }
   
   var oReq = new XMLHttpRequest();
