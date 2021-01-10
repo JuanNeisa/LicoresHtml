e2bd29b9045eacd41f741cac31768299aff7f7ac
Juan David Neisa Paramo <jdneisap@correo.udistrital.edu.co>
2021-01-09 22:35:06 -0500

    Agregar objetos al DOM mediante JS y mapeo de un objeto

--- Ajax-ejemplo/ajax.js
+++ Ajax-ejemplo/ajax.js
@@ -6,7 +6,17 @@ function reqListener () {
     //console.log(this.responseText);
 
     //Se puede castear los objetos a formato JSON
-    console.log(JSON.parse(this.responseText));
+    //Ademas se puede adignar el objeto a una variable
+    var usuarios =JSON.parse(this.responseText);
+
+    //Mapeadno el objeto JSON para listarlo
+    var listaUsuarios = usuarios.map(x => `<li>${x.nombre}</li>`).join('');
+    //Mostrar en consola el listado
+    console.log(listaUsuarios)
+    
+    //Agregar el listado de usuarios mediante 'innherHTML'
+    lista.innerHTML=listaUsuarios;
+
   }
   
   var oReq = new XMLHttpRequest();
