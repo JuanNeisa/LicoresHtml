b41b9db0ce840e2c577ed8ab5ae7bf04c294a9e5
Juan David Neisa Paramo <jdneisap@correo.udistrital.edu.co>
2021-01-09 22:11:34 -0500

    Estructura peticion XMLHttpRequest

--- Ajax-ejemplo/ajax.js
+++ Ajax-ejemplo/ajax.js
@@ -1,1 +1,10 @@
-console.log('Ejemplo ajax')
+function reqListener () {
+    //La peticion llega como texto en el momento que se usa 'responseText'
+    console.log(this.responseText);
+  }
+  
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener);
+  //oReq.open("[Tipo_Peticion]", "Direccion_Servidor");
+  oReq.open("GET", "https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios");
+  oReq.send();
